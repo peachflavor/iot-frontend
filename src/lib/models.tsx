6e19eaa8e22f230496abd6e9c761b8bc2e61b1e2
peachflavor <65070055@kmitl.ac.th>
2024-07-31 18:36:28 +0700

    Update models.tsx

--- src/lib/models.tsx
+++ src/lib/models.tsx
@@ -3,5 +3,25 @@ export interface Book {
   title: string;
   author: string;
   year: number;
+  detail: string;
+  story: string;
+  classification: string;
   is_published: boolean;
+}
+
+export interface Menu {
+  id: number;
+  name: string;
+  price: number;
+  detail: string;
+  ingredient: string;
+  is_published: boolean;
+}
+
+export interface Order {
+  id: number;
+  name: string;
+  total: number;
+  price: number;
+  note: string;
 }
