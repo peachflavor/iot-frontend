705dff14b4167e71e2455aef15aceb38bccbadd2
peachflavor <65070055@kmitl.ac.th>
2024-07-31 18:36:23 +0700

    Update layout.tsx

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -11,32 +11,41 @@ export default function Layout({ children }: LayoutProps) {
     <>
       <Box>
         <header className="h-14 px-8 border border-t-0 border-x-0 border-solid border-neutral-200 bg-white">
-          <div className="flex justify-between">
-            <div>
-              <img src={websiteLogo} alt="Logo" className="h-14 w-auto" />
-            </div>
+          <div className="flex justify-between items-center h-full">
+            <img src={websiteLogo} alt="Website Logo" className="h-14 w-auto" />
 
-            <Group className="h-14 gap-0">
+            <Group className="h-full gap-4">
               <Link
-                to={"/"}
-                className="flex items-center h-14 px-1 no-underline text-neutral-600 font-semibold text-sm"
+                to="/"
+                className="flex items-center h-full px-2 no-underline text-neutral-600 font-semibold text-sm"
+                aria-label="Home"
               >
                 หน้าหลัก
               </Link>
 
               <Link
-                to={"/books"}
-                className="flex items-center h-14 px-1 no-underline text-neutral-600 font-semibold text-sm"
+                to="/books"
+                className="flex items-center h-full px-2 no-underline text-neutral-600 font-semibold text-sm"
+                aria-label="Books"
               >
                 หนังสือ
               </Link>
 
-              {/* <Link
-                to={"/menu"}
-                className="flex items-center h-14 px-1 no-underline text-neutral-600 font-semibold text-sm"
+              <Link
+                to="/menus"
+                className="flex items-center h-full px-2 no-underline text-neutral-600 font-semibold text-sm"
+                aria-label="Menus"
               >
                 เมนู
-              </Link> */}
+              </Link>
+
+              <Link
+                to="/staffs"
+                className="flex items-center h-full px-2 no-underline text-neutral-600 font-semibold text-sm"
+                aria-label="Staff"
+              >
+                สตาฟ
+              </Link>
             </Group>
 
             <div></div>
