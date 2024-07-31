9579245908b9e6125b09a25d429528779cb33ae3
peachflavor <65070055@kmitl.ac.th>
2024-07-31 10:45:42 +0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function HomePage() {
           backgroundImage: `url(${cafeBackgroundImage})`,
         }}
       >
-        <h1 className="text-5xl mb-2">ยินดีต้อนรับสู่ IoT Library & Cafe</h1>
+        <h1 className="text-5xl mb-2">Welcome to IoT Library & Cafe</h1>
         <h2>ร้านกาแฟที่มีหนังสืออยู่นิดหน่อยให้คุณได้อ่าน</h2>
       </section>
 
